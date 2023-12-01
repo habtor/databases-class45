0f43c73b8d815aebf9f025df45359016e4d43efd
Saleh Habtor <saleh.tiger2@gmail.com>
2023-12-01 21:59:21 +0100

    responded to comments part 1

--- Week2/exe1-keys.js
+++ Week2/exe1-keys.js
@@ -14,31 +14,23 @@ connection.connect((err) => {
 });
 
 //============ Delete, create and select database if exist ============
-const deleteDatabase = "DROP DATABASE IF EXISTS books";
-const createDatabase = "CREATE DATABASE IF NOT EXISTS books";
-const useDatabase = "USE books";
-//=========== Create tables ============
-const createAuthorTable = `CREATE TABLE authors(
+
+const queries = [
+  "DROP DATABASE IF EXISTS books",
+  "CREATE DATABASE IF NOT EXISTS books",
+  "USE books",
+  `CREATE TABLE authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(255),
     university VARCHAR(255),
     date_of_birth DATE,
     h_index INT,
     gender VARCHAR(20)
-    )`;
-
-//====== Update authors table
-const updateAuthorTable = `ALTER TABLE authors
+    )`,
+  `ALTER TABLE authors
     ADD COLUMN mentor INT,
     ADD CONSTRAINT foreignKey_mentor
-    FOREIGN KEY (mentor) REFERENCES authors(author_id);`;
-
-const queries = [
-  deleteDatabase,
-  createDatabase,
-  useDatabase,
-  createAuthorTable,
-  updateAuthorTable,
+    FOREIGN KEY (mentor) REFERENCES authors(author_id);`,
 ];
 
 queries.forEach((query) => {
